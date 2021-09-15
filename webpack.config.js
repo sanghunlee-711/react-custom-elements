c546cf14b275aac1466426c13c41262de95500bc
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-09-15 14:53:17 +0900

    [Modify] webpack-config

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 let mode = 'development';
 let target = 'web';
-const plugins = [
-  new CleanWebpackPlugin(),
-  // new MiniCssExtractPlugin(),
-  // new HtmlWebpackPlugin({
-  //   template: './src/index.html',
-  // }),
-];
+const plugins = [new CleanWebpackPlugin()];
 
 if (process.env.NODE_ENV === 'production') {
   mode = 'production';
@@ -43,11 +37,6 @@ module.exports = {
       {
         test: /\.(s[ac]|c)ss$/i,
         use: [
-          // {
-          // loader: MiniCssExtractPlugin.loader,
-          // This is required for asset imports in CSS, such as url()
-          // options: { publicPath: '' },
-          // },
           //웹팩 로더는 한 파일에 대해 여러가지가 실행되는데 배열의 뒤에서부터 앞으로 작동한다.
           'style-loader', // js로 생성된 css를 styles 노드로 생성
           'css-loader', // css 를 js로
@@ -82,7 +71,7 @@ module.exports = {
         exclude: /node_modules/,
         use: [{ loader: 'ts-loader' }],
       },
-      // 모든 '.js' 출력 파일은 'source-map-loader'에서 다시 처리한 소스 맵이 있습니다.
+      // 모든 '.js' 출력 파일은 'source-map-loader'에서 다시 처리한 소스 맵에서 존재.
       {
         enforce: 'pre',
         test: /\.js$/,
@@ -119,6 +108,7 @@ module.exports = {
     contentBase: './dist',
     hot: true,
   },
+  //externals를 사용하게되면 React 가 referenced 되지 않는 에러 발생
   // externals: {
   //   react: 'React',
   //   'react-dom': 'ReactDOM',
