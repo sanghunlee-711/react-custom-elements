aa0b3dc7b92108de67c4cac3dbe3146b3271dada
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-09-15 16:42:47 +0900

    [Modify] babelconfig

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
-// Cannot load "react-refresh/babel" in production
 const plugins = [];
+// Cannot load "react-refresh/babel" in production
 if (process.env.NODE_ENV !== 'production') {
   plugins.push('react-refresh/babel');
 }
@@ -7,8 +7,8 @@ if (process.env.NODE_ENV !== 'production') {
 module.exports = {
   presets: [
     '@babel/preset-env',
-    // Runtime automatic with React 17+ allows not importing React
-    // in files only using JSX (no state or React methods)
+    // Runtime automatic with React 17+ allows not importing React // 근데 왜 안됨 ;
+    // in files only using JSX ts가 안되는건가 ?..(no state or React methods)
     ['@babel/preset-react', { runtime: 'automatic' }],
   ],
   plugins: plugins,
